e08f761886b8dcd8b603e56b1fc44ba166f5cb65
prathmeshmahure <103621463+prathmeshmahure@users.noreply.github.com>
2023-11-16 17:03:20 +0530

    changes

--- src/main/java/com/utility/base_class.java
+++ src/main/java/com/utility/base_class.java
@@ -209,7 +209,7 @@ public class base_class {
 					cap.setCapability("appium:avdLaunchTimeout", 600000);		
 					cap.setCapability("appPackage", "com.heerofreedo");               //freedo
 					cap.setCapability("appActivity", "com.heerofreedo.MainActivity"); //freedo
-					cap.setCapability("appium:noReset", "true");
+					cap.setCapability("appium:noReset", "false");
 					
 					cap.setCapability("appium:app",(System.getProperty("user.dir")+"\\apk\\10app-release.apk"));
 					cap.setCapability("appium:ensureWebviewsHavePages", true);
